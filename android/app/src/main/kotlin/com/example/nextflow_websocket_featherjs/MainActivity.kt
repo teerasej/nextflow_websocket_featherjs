0e97e2ea9717b76e348c046b9bcf37147271b648
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-01-15 09:43:59 +0700

    1st commit with able to call service with feather

---
+++ android/app/src/main/kotlin/com/example/nextflow_websocket_featherjs/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nextflow_websocket_featherjs
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_feathersjs/flutter_feathersjs.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  FlutterFeathersjs flutterFeathersjs = FlutterFeathersjs()
+    ..init(baseUrl: 'http://175c-183-88-190-139.ngrok.io');
+
+  void _incrementCounter() async {
+    var serviceName = 'api/auction_sequence_announcement';
+    var messageResponse = await flutterFeathersjs.find(
+      serviceName: serviceName,
+      query: {
+        'auction_sequence_id': 1,
+      },
+    );
+
+    print(messageResponse);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Container(),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: _incrementCounter,
+            tooltip: 'Increment',
+            child: const Text('feather'),
+          ),
+          FloatingActionButton(
+            onPressed: _socketIO,
+            tooltip: 'Increment',
+            child: const Text('socket io'),
+          ),
+        ],
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  void _socketIO() {}
+}
